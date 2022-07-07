e650aec55f1028becacd210d32fc45c236e261a8
venkatasai510 <venkatasai@sketchbrahma.com>
2022-07-07 12:37:28 +0530

    Component Hello wrote with JSX

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './Components/greet';
 import Welcome from './Components/welcome';
+import Hello from './Components/hello';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome /> 
+      {/* <Greet />
+      <Welcome />  */}
+      <Hello />
     </div>
   );
 }
---
+++ src/Components/hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelloJsx = () => {
+    return(
+        <div>
+            <h1>Hello Jsx Compiler</h1>
+        </div>
+    )
+}
+
+export default HelloJsx;
