27dda45d380d31299062c5f5ffc4907bbe3075d4
venkatasai510 <venkatasai@sketchbrahma.com>
2022-07-07 17:20:36 +0530

    SetState method with a update function

--- src/Components/counter.js
+++ src/Components/counter.js
@@ -11,22 +11,33 @@ class Counter extends Component {
 
   increment() {
     // this.state.count = this.state.count + 1
-    this.setState(
-      {
-        count: this.state.count + 1,
-      },
-      () => {
-        console.log("callback Value", this.state.count);
-      }
-    );
+    // this.setState(
+    //   {
+    //     count: this.state.count + 1,
+    //   },
+    //   () => {
+    //     console.log("callback Value", this.state.count);
+    //   }
+    // );
+    this.setState((prevState, props) => ({
+        count : prevState.count + 1
+    }))
     console.log(this.state.count);
+  }
+
+  incrementFive(){
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
   }
 
   render() {
     return (
       <div>
         Count - {this.state.count}
-        <button onClick={() => this.increment()}>Increment</button>
+        <button onClick={() => this.incrementFive()}>Increment</button>
       </div>
     );
   }
