fa71988d066d76b2b7f9e38408cd9fd9b01c586b
venkatasai510 <venkatasai@sketchbrahma.com>
2022-07-07 12:27:56 +0530

    Class Component function in the component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './Components/greet';
+import Welcome from './Components/welcome';
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Welcome /> 
     </div>
   );
 }
