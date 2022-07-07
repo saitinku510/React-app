eb660671036a73a29d7a2c951332c9d25bbb915e
venkatasai510 <venkatasai@sketchbrahma.com>
2022-07-07 12:48:40 +0530

    Component Hello wrote without JSX => Normal createElement function

--- src/Components/hello.js
+++ src/Components/hello.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const HelloJsx = () => {
-    return(
-        <div>
-            <h1>Hello Jsx Compiler</h1>
-        </div>
-    )
-}
+  // return(
+  //     <div className = "hello" id="Hey">
+  //         <h1>Hello Jsx Compiler</h1>
+  //     </div>
+  // )
+  return React.createElement(
+    "div",
+    { id: "hey", className: "hello" },
+    React.createElement("h1", null, "Hello Me")
+  );
+};
 
 export default HelloJsx;
