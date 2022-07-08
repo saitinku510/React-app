8542c205c1d0e9371caaa3bcba869d5c324554b9
venkatasai510 <venkatasai@sketchbrahma.com>
2022-07-08 11:13:31 +0530

    List rendering of an array in a loop method

--- src/Components/nameList.js
+++ src/Components/nameList.js
@@ -1,14 +1,9 @@
-import React from 'react'
+import React from "react";
 
 function NameList() {
-    const Names = ['venkat', 'sadik', 'bhargav']
-  return (
-    <div>
-      <h2>{Names[0]}</h2>
-      <h2>{Names[1]}</h2>
-      <h2>{Names[2]}</h2>
-    </div>
-  )
+  const Names = ["venkat", "sadik", "bhargav"];
+  const list = Names.map((name) => <h2>{name}</h2>);
+  return <div>{list}</div>;
 }
 
-export default NameList
+export default NameList;
