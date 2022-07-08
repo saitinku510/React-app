c238779cf9a340f0a59444d8de4b6c5d30e9fd9d
venkatasai510 <venkatasai@sketchbrahma.com>
2022-07-08 15:28:04 +0530

    Created a form input element can be edited

--- src/Components/form.js
+++ src/Components/form.js
@@ -6,25 +6,63 @@ class Form extends Component {
 
     this.state = {
       username: "",
+      comments: "",
+      select: "angular",
     };
   }
-  handleUserName = (event) => {
+
+  handleUserName = event => {
     this.setState({
       username: event.target.value,
     });
   };
+
+  handleComments = event => {
+    this.setState({
+      comments: event.target.value,
+    });
+  };
+
+  handleSelect = event => {
+    this.setState({
+      select: event.target.value,
+    });
+  };
+  
+  handleSubmit = event => {
+      event.preventDefault()
+      alert(`${this.state.username} ${this.state.select} ${this.state.comments}`)
+  }
+
   render() {
+      const {comments, username, select} = this.state
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div>
             <label>Name</label>
             <input
               type="text"
-              value={this.state.username}
+              value={username}
               onChange={this.handleUserName}
             />
           </div>
+          <div>
+            <label>Comments</label>
+            <textarea
+              value={comments}
+              onChange={this.handleComments}
+            ></textarea>
+          </div>
+          <div>
+            <label>Select</label>
+            <select value={select} onChange={this.handleSelect}>
+              <option value="react">React</option>
+              <option value="angular">Angular</option>
+              <option value="vue">Vue</option>
+            </select>
+          </div>
+          <button>Submit</button>
         </form>
       </div>
     );
